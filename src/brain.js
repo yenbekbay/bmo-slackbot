264ec25b872f9b3366944ff7fe25e8c579b3cd6c
Ayan Yenbekbay <ayan.yenb@gmail.com>
2016-04-25 21:08:27 -0500

    Store users and channels in redis

--- src/brain.js
+++ src/brain.js
@@ -1,51 +1,130 @@
 'use strict';
 
 const Rx = require('rx-lite');
+const stringify = require('json-stringify-safe');
 
 class Brain {
   constructor(redisClient, logger) {
     this.redisClient = redisClient;
+    this.logger = logger;
   }
 
-  getLastVotedUser(channel) {
-    return this
-      ._runCommand('hget', 'last_voted_users', channel)
-      .doOnError(err => this.logger.error(
-        `Failed to get last voted user for channel ${channel}: ${err}`
-      ));
+  getUsers() {
+    return this._getObjects('user');
   }
 
-  setLastVotedUser(channel, username) {
-    return this
-      ._runCommand('hset', 'last_voted_users', channel, username)
-      .doOnError(err => this.logger.error(
-        `Failed to set last voted user for channel ${channel}: ${err}`
-      ));
+  saveUsers(users) {
+    return this._saveObjects('user', users);
   }
 
-  getUserScore(username) {
+  getUser(userId) {
+    return this._getObject('user', userId);
+  }
+
+  getChannels() {
+    return this._getObjects('channel');
+  }
+
+  saveChannels(channels) {
+    return this._saveObjects('channel', channels);
+  }
+
+  getChannel(channelId) {
+    return this._getObject('channel', channelId);
+  }
+
+  getLastVotedUser(channelId) {
     return this
-      ._runCommand('hget', 'user_scores', username)
+      ._runCommand('hget', 'last_voted_users', channelId)
+      .doOnError(err => this.logger
+        .error(`Failed to get last voted user for channel ${channelId}: ${err}`)
+      )
+      .doOnNext(userId => this.logger
+        .debug(`Found last voted user for channel ${channelId}: ${userId}`)
+      );
+  }
+
+  setLastVotedUser(channelId, userId) {
+    return this
+      ._runCommand('hset', 'last_voted_users', channelId, userId)
+      .doOnError(err => this.logger
+        .error(`Failed to set last voted user for channel ${channelId}: ${err}`)
+      )
+      .doOnNext(created => this.logger
+        .debug(`Updated last voted user for channel ${channelId}: ${userId}`)
+      );
+  }
+
+  getUserScore(userId) {
+    return this
+      ._runCommand('hget', 'user_scores', userId)
       .map(score => score || 0)
-      .doOnError(err => this.logger.error(
-        `Failed to get score for user ${username}: ${err}`
-      ));
+      .doOnError(err => this.logger
+        .error(`Failed to get score for user ${userId}: ${err}`)
+      )
+      .doOnNext(score => this.logger
+        .debug(`Found score for user ${userId}: ${score}`)
+      );
   }
 
   getUserScores() {
     return this
       ._runCommand('hgetall', 'user_scores')
-      .doOnError(err => this.logger.error(
-        `Failed to get user scores: ${err}`
-      ));
+      .doOnError(err => this.logger
+        .error(`Failed to get user scores: ${err}`)
+      )
+      .doOnNext(scores => this.logger
+        .debug(scores
+          ? `Found user scores: ${stringify(scores)}`
+          : 'Found no user scores'
+        )
+      );
   }
 
-  incrementUserScore(username, points) {
+  incrementUserScore(userId, points) {
     return this
-      ._runCommand('hincrby', 'user_scores', username, points)
-      .doOnError(err => this.logger.error(
-        `Failed to increment score for user ${username}: ${err}`
-      ));
+      ._runCommand('hincrby', 'user_scores', userId, points)
+      .doOnError(err => this.logger
+        .error(`Failed to increment score for user ${userId}: ${err}`)
+      )
+      .doOnNext(score => this.logger
+        .debug(`Incremented score for user ${userId} by ${points}: ${score}`)
+      );
+  }
+
+  _getObjects(key) {
+    return this
+      ._runCommand('keys', `${key}_*`)
+      .flatMap(keys => this._runBatch(keys.map(key => ['hgetall', key])))
+      .doOnError(err => this.logger.error(`Failed to get ${key}s: ${err}`))
+      .doOnNext(objects => this.logger
+        .debug(`Found ${objects.length} ${key}s`)
+      );
+  }
+
+  _saveObjects(key, objects) {
+    return this
+      ._runBatch(objects
+        .map(object => ['hmset', `${key}_${object.id}`, object])
+      )
+      .doOnError(err => this.logger.error(`Failed to save ${key}s: ${err}`))
+      .doOnNext(objects => this.logger
+        .debug(`Saved ${objects.length} ${key}s`)
+      );
+  }
+
+  _getObject(key, objectId) {
+    return this
+      ._runCommand('hgetall', `${key}_${objectId}`)
+      .doOnError(err => this.logger
+        .error(`Failed to get ${key} for id ${objectId}: ${err}`)
+      )
+      .doOnNext(object => this.logger
+        .debug(object
+          ? `Found ${key} for id ${objectId}: ${stringify(object)}`
+          : `Found no ${key} for id ${objectId}`
+        )
+      );
   }
 
   _runCommand() {
@@ -54,6 +133,11 @@ class Brain {
       this.redisClient[args[0]],
       this.redisClient
     )(...args.slice(1));
+  }
+
+  _runBatch(commands) {
+    const batch = this.redisClient.batch(commands);
+    return Rx.Observable.fromNodeCallback(batch.exec, batch)();
   }
 }
 
